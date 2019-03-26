a2bd5fc39e0efeb528741b7e8565bea48e5936d1
David McDonald <info@davidmcdonald.org>
2019-03-26 12:17:45 +1100

    tweaks search form

--- searchform.php
+++ searchform.php
@@ -1,11 +1,8 @@
 <form role="search" method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<div class="row collapse">
-		<div class="medium-8 small-9 columns">
-			<input type="text" value="" name="s" id="s" placeholder="<?php esc_attr_e( 'Search', 'dmcstarter' ); ?>">
-		</div>
-		<div class="medium-4 small-3 columns">
-			<input type="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', 'dmcstarter' ); ?>" class="button postfix">
-		</div>
+	<div class="single-field-search">
+		<i class="fal fa-search"></i>
+		<input type="text" value="<?php echo get_search_query(); ?>" name="s" id="s" placeholder="<?php esc_attr_e( 'Search', 'dmcstarter' ); ?>">
+		<input type="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', 'dmcstarter' ); ?>">
 	</div>
 </form>
 
