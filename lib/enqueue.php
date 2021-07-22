20839f36e6b757a02170d5d638aa7ad58e35a551
David McDonald <info@davidmcdonald.org>
2021-07-22 12:12:16 +1000

    adds to WC dequeue scripts and styles list

--- lib/enqueue.php
+++ lib/enqueue.php
@@ -114,22 +114,37 @@ add_filter( 'style_loader_tag', 'dmc_google_font_loader_tag_filter', 10, 2 );
 
 
 // only load WooCommerce styles and scripts on WooCommerce pages
-// function dmc_conditionally_load_woc_js_css() {
-// 	if ( ! is_woocommerce() && ! is_cart() && ! is_checkout() && ! is_account_page() ) {
+function dmc_conditionally_load_woc_js_css() {
+	if ( ! is_woocommerce() && ! is_cart() && ! is_checkout() && ! is_account_page() ) {
 
-// 		## Dequeue scripts.
-// 		wp_dequeue_script( 'woocommerce' );
-// 		wp_dequeue_script( 'wc-add-to-cart' );
-// 		wp_dequeue_script( 'wc-cart-fragments' );
+		## Dequeue scripts.
+		wp_dequeue_script( 'woocommerce' );
+		wp_dequeue_script( 'wc-add-to-cart' );
+		wp_dequeue_script( 'wc-cart-fragments' );
+		wp_dequeue_script( 'wc_price_slider' );
+		wp_dequeue_script( 'wc-single-product' );
+		wp_dequeue_script( 'wc-add-to-cart' );
+		wp_dequeue_script( 'wc-cart-fragments' );
+		wp_dequeue_script( 'wc-checkout' );
+		wp_dequeue_script( 'wc-add-to-cart-variation' );
+		wp_dequeue_script( 'wc-single-product' );
+		wp_dequeue_script( 'wc-cart' );
+		wp_dequeue_script( 'wc-chosen' );
+		wp_dequeue_script( 'prettyPhoto' );
+		wp_dequeue_script( 'prettyPhoto-init' );
 
-// 		## Dequeue styles.
-// 		wp_dequeue_style( 'woocommerce-general' );
-// 		wp_dequeue_style( 'woocommerce-layout' );
-// 		wp_dequeue_style( 'woocommerce-smallscreen' );
+		## Dequeue styles.
+		wp_dequeue_style( 'woocommerce-general' );
+		wp_dequeue_style( 'woocommerce-layout' );
+		wp_dequeue_style( 'woocommerce-smallscreen' );
+		wp_dequeue_style( 'woocommerce_frontend_styles' );
+		wp_dequeue_style( 'woocommerce_fancybox_styles' );
+		wp_dequeue_style( 'woocommerce_chosen_styles' );
+		wp_dequeue_style( 'woocommerce_prettyPhoto_css' );
 
-// 	}
-// }
-// add_action( 'wp_enqueue_scripts', 'dmc_conditionally_load_woc_js_css' );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'dmc_conditionally_load_woc_js_css' );
 
 
 // function dmc_load_wc_js_css_exceptions() {
