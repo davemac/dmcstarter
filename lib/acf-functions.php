253e856b77b3d9104d12b9efb31b740bad4d552d
David McDonald <info@davidmcdonald.org>
2022-03-01 11:36:52 +1100

    adds hide acf admin function to non-production functions

--- lib/acf-functions.php
+++ lib/acf-functions.php
@@ -1,25 +1,5 @@
 <?php
 
-// Hide ACF in admin menu for certain URLs
-function dmc_hide_acf_admin() {
-	$site_url = get_bloginfo( 'url' );
-
-	$protected_urls = array(
-		'https://prod.com.au',
-		'https://staging.com.au',
-	);
-
-	if ( in_array( $site_url, $protected_urls, true ) ) {
-		// Hide the ACF menu item
-		return false;
-
-	} else {
-		return true;
-	}
-}
-add_filter( 'acf/settings/show_admin', 'dmc_hide_acf_admin' );
-
-
 // adds ACF options page
 if ( function_exists( 'acf_add_options_page' ) ) {
 	acf_add_options_page(
--- lib/non-production.php
+++ lib/non-production.php
@@ -9,6 +9,27 @@ if ( 'production' !== wp_get_environment_type() ) :
 
 	// Enable "Discourage search engines from indexing this site" option.
 	add_filter( 'pre_option_blog_public', '__return_zero', 999 );
+
+
+	// Hide ACF in admin menu for prod/staging URLs
+	function dmc_hide_acf_admin() {
+		$site_url = get_bloginfo( 'url' );
+
+		$protected_urls = array(
+			'https://prod.com.au',
+			'https://staging.com.au',
+		);
+
+		if ( in_array( $site_url, $protected_urls, true ) ) {
+			// Hide the ACF menu item
+			return false;
+
+		} else {
+			return true;
+		}
+	}
+	add_filter( 'acf/settings/show_admin', 'dmc_hide_acf_admin' );
+
 endif;
 
 
