b15c7e2f1be13f33785cbc57f3cbdc788d233142
David McDonald <info@davidmcdonald.org>
2022-06-22 16:41:56 +1000

    adds social icons font

--- lib/socials.php
+++ lib/socials.php
@@ -1,13 +1,15 @@
-<div class="social__wrap">
-	<div class="social__item">
-		<a class="social__link--facebook" href="#" target="_blank" rel="noopener">
-			<span class="social__link--text">facebook</span>
-		</a>
-	</div>
+<div class="socials">
 
-	<div class="social__item">
-		<a class="social__link--instagram" href="#" target="_blank" rel="noopener">
-			<span class="social__link--text">instagram</span>
-		</a>
-	</div>
+	<h4>
+		Follow Us
+	</h4>
+
+	<a href="https://www.instagram.com/queenslanderaviaries" class="icon-instagram" href=" target="_blank" rel="noopener"></a>
+
+	<a href="https://www.facebook.com/queenslanderaviaries" class="icon-facebook" href=" target="_blank" rel="noopener"></a>
+
+	<a href="https://www.youtube.com/channel/UCVVZpX7IpnXp7mh03vMi3ZQ" class="icon-youtube" href=" target="_blank" rel="noopener"></a>
+
+	<a href="https://twitter.com/QlderAviaries" class="icon-twitter" href=" target="_blank" rel="noopener"></a>
+
 </div>
